76d8e45782003a862b78b324354fd8d6b0ccd5f0
Tomke <pfoch.tomke@gmail.com>
2024-06-04 13:59:05 +0200

    inos

--- Motor.hpp
+++ Motor.hpp
@@ -1,9 +1,13 @@
 #pragma once
+#include <math.h>
+#ifndef PI
 #define PI 3.14159265358979
+#endif
 class Motor{
     public:
         // return inaccuarrcy
         virtual double setAngle(double angle)=0;
+        virtual double update()=0;
 };
 
 // class MotorStepper : public Motor{
@@ -31,17 +35,23 @@ class Motor{
 
 class MotorServo : public Motor {
         Servo s;
-        int pin;
+        uint8_t pin;
         double currentAngle=0,
             speed;
         public:
-            Servo(int pin, double speed=100) : pin(pin), speed(speed)
+            MotorServo(uint8_t pin, double speed=100) : pin(pin), speed(speed)
             {
                 s.attach(pin);
             }
-            void setAngle(double angle) override
+            double setAngle(double angle) override
             {
-                s.write(angle*180/PI);
-                delay((angle-currentAngle)*1000/speed);
+              Serial.println(angle);
+                for(int i = currentAngle; i < angle; i++){
+                  s.write(i);
+                  delay(1000/speed);
+                }
+                currentAngle=angle;
+                return 0;
             }
+        double update() override {};
 };
