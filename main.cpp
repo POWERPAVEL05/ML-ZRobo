f777d2857a36274614f71e78f0ce96d97f777831
Tomke <pfoch.tomke@gmail.com>
2024-07-31 10:10:06 +0200

    read COM

--- main.cpp
+++ main.cpp
@@ -2,18 +2,16 @@
 
 #define cm
 
+DrawRobot<Arm> robot;
+
 void setup()
 {
     Serial.begin(115200);
 
     Motor root(AccelStepper(8,4,6,5,7), 4076);
     Motor ext(AccelStepper(8,8,9,10,11), 4076);
-    Motor dummy(AccelStepper(1,23,24), 200, 4000);
 
-    // ext.stepper.runToNewPosition(ext.getSteps(180));
-    // root.stepper.runToNewPosition(ext.getSteps(180));
-
-    DrawRobot<Arm> robot = {
+    robot = {
     .axies = Arm(11 cm, 11.5 cm, root, ext),
         .gantry = Gantry(6, 7)
     };
@@ -21,9 +19,22 @@ void setup()
     // robot.axies.moveTo({22.5,0});
     robot.axies.moveTo({11,11.5});
 
-    while(true)
-        ;
 }
 void loop()
 {
+    int cmd = Serial.parseInt();
+    switch(cmd){
+        case 0:{
+            int x = Serial.parseInt();
+            int y = Serial.parseInt();
+            robot.axies.moveTo({x,y});
+            break;
+        }
+        case 1: {
+            int tool = Serial.parseInt();
+            robot.gantry.setTool(tool); // ist tool
+        }
+
+    }
+
 }
